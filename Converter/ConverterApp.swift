83694b7fbe5e1a59d080e73eb6658b0869833fd7
Orlando Carmona <orcabe86@gmail.com>
2022-04-06 21:29:59 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Hector Carmona on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
